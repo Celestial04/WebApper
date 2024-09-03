45a5f5f636606112880e108988e6e38e117ec54b
celeste <hugococa2004@gmail.com>
2024-09-03 19:35:34 +0200

    alignement changé pour les contenus du tuto

--- app/src/main/java/com/bouillie/web/tuto_1.java
+++ app/src/main/java/com/bouillie/web/tuto_1.java
@@ -4,27 +4,20 @@ import static android.app.usage.UsageEvents.Event.NONE;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.boullie.web.R;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.color.DynamicColors;
-import com.google.android.play.core.appupdate.AppUpdateInfo;
-import com.google.android.play.core.appupdate.AppUpdateManager;
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
-import com.google.android.play.core.install.model.AppUpdateType;
-import com.google.android.play.core.install.model.UpdateAvailability;
 
 public class tuto_1 extends AppCompatActivity {
-    private int selectedTheme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         int savedTheme = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getInt("selected_theme", NONE);
 
+        int selectedTheme;
         if (savedTheme != NONE) {
             selectedTheme = savedTheme;
         }else{
@@ -33,24 +26,6 @@ public class tuto_1 extends AppCompatActivity {
         setTheme(selectedTheme);
         DynamicColors.applyToActivitiesIfAvailable(this.getApplication());
         setContentView(R.layout.tuto_1);
-        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(this);
-
-// Returns an intent object that you use to check for an update.
-        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
-
-// Checks that the platform will allow the specified type of update.
-        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
-                appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE);
-            }// This example applies an immediate update. To apply a flexible update
-// instead, pass in AppUpdateType.FLEXIBLE
-// Request the update.
-        });
-        DynamicColors.applyToActivitiesIfAvailable(getApplication());
-        if (getIntent().getBooleanExtra("EXIT", false)) {
-            finish();
-
-        }
         Button btn = findViewById(R.id.button4);
         btn.setOnClickListener(v -> {
             Intent intent = new Intent(tuto_1.this, tuto_2.class);
@@ -58,19 +33,6 @@ public class tuto_1 extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
-    }
-
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LOW_PROFILE);
-        }
-
     }
 
 }
